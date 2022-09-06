aee8d50e4e0576ab5024368c5734ca675e6a5d0f
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-09-07 01:41:44 +0900

    postgresqldb

--- config/production.py
+++ config/production.py
@@ -1,7 +1,16 @@
 from logging.config import dictConfig
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'balllab.db'))
+from dotenv import load_dotenv
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'2\xe1<C,\x17\xd5\x99\x9exSU\x0el\x0c\x91'
 

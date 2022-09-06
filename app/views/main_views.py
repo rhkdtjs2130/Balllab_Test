709aff82c9a12699445465586604930efa843b4f
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-09-06 11:48:46 +0900

    checkpay

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -398,7 +398,7 @@ def get_qrcode(email, date, time):
 @bp.route("/pay_check", methods=["POST"])
 def pay_check():
     print(request.form)
-    print(request.form['var2'])
+    print(request.form['var1'])
     key_info = "3c0VLPJBsy0//kO2e3TEe+1DPJnCCRVaOgT+oqg6zaM="
     value_info = "3c0VLPJBsy0//kO2e3TEexga0slLAiui2bsP1P985Rc="
     

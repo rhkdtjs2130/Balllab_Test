39ab4dee9b6fce78537fed2ed31d22f328fccd82
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-09-07 01:12:34 +0900

    add logging

--- config/production.py
+++ config/production.py
@@ -1,5 +1,30 @@
+from logging.config import dictConfig
 from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'balllab.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'2\xe1<C,\x17\xd5\x99\x9exSU\x0el\x0c\x91'
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})

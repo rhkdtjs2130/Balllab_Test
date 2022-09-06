4ca29db9022b5667651e2afb8bc732badea22adc
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-09-07 00:04:56 +0900

    add

--- config/production.py
+++ config/production.py
@@ -1,5 +1,5 @@
 from config.default import *
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'balllab.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'2\xe1<C,\x17\xd5\x99\x9exSU\x0el\x0c\x91'

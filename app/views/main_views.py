ff080741557e567dc60fbb991f47698cf60291eb
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-09-06 18:58:52 +0900

    check

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -390,8 +390,8 @@ def request_pay_court(email, date, area, time, court, total_price):
             if paycheck[0].pay_state == "4":
                 for reservation in reservation_table:
                     file_name = url_for("static", filename=f"qr_code/{email}_{area}_{date}_{reservation.time}.png")
-                    reservation['pay'] = 1
-                    reservation['qr_path'] = file_name
+                    reservation.pay = 1
+                    reservation.qr_path = file_name
                     
                     db.session.commit()
                     

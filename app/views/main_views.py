93ca470368c4b2288b87b29c1a155fb59d8b7e53
KyungHyun Jovinus Lee <lkh256@gmail.com>
2022-09-06 18:34:13 +0900

    check

--- app/views/main_views.py
+++ app/views/main_views.py
@@ -364,7 +364,8 @@ def request_pay_court(email, date, area, time, court, total_price):
     #     resp = urllib.parse.unquote_to_bytes(f.read())
     #     resp = resp.decode('utf-8')[6]
     #     print("TEST", "State = ", resp, "Test")
-    print("Date = ",date)
+    print("Date = ", date)
+    print("Date = ", datetime.datetime.strptime(date, '%Y-%m-%d'))
     print(time)
     print(court)
     print(area)
